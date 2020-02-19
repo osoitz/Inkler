52450023d8bf9c46b07280472764ce918d03d251
Osoitz <oelkoro@gmail.com>
2020-02-19 13:13:51 +0100

    Habemus webs

--- app/src/main/java/com/example/inkler/FichaTatuadorActivity.java
+++ app/src/main/java/com/example/inkler/FichaTatuadorActivity.java
@@ -241,6 +241,40 @@ public class FichaTatuadorActivity extends AppCompatActivity {
         return websTatuador;
     }
 
+    private ArrayList<String> recogerWebsEstudio (String id) {
+        ArrayList<String> webs = new ArrayList<>();
+        // Iniciar base de datos
+        DBHelper dbHelper = new DBHelper(getBaseContext());
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        //Columnas
+        String[] projection = {
+                DBHelper.entidadWeb.COLUMN_NAME_URL
+        };
+
+        String selection = DBHelper.entidadWeb.COLUMN_NAME_ID_ESTUDIO + " = ?";
+        String[] selectionArgs = new String[] { "" + id } ;
+        //Respuesta
+
+        Cursor cursor = db.query(
+                DBHelper.entidadWeb.TABLE_NAME,
+                projection,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                null);
+        ;
+        while(cursor.moveToNext()) {
+            String url = cursor.getString(
+                    cursor.getColumnIndexOrThrow(DBHelper.entidadWeb.COLUMN_NAME_URL));
+            webs.add(url);
+        }
+        cursor.close();
+        dbHelper.close();
+        return webs;
+    }
+
     private void rellenar_txt(Tatuador miTatuador, Estudio miestudio){
         TextView nombreArtistico = findViewById(R.id.nombreArtistico);
         TextView nombreTatuador =findViewById(R.id.nombreApellidos);
@@ -259,20 +293,32 @@ public class FichaTatuadorActivity extends AppCompatActivity {
         telefonoEstudio.setText(miestudio.getTelefono());
     }
 
-    private void rellenarWebsTatuador(ArrayList<String> urlsTatuador){
+    private String crearContenidoHTML(ArrayList<String> urls){
         String contenidoCampo ="";
-        for (String urlString : urlsTatuador){
+        for (String urlString : urls){
+            String host = urlString;
             try {
                 URL miUrl = new URL(urlString);
-                contenidoCampo = contenidoCampo + "<a href='" + urlString + "'>"+ miUrl.getHost() +"</a><br>";
+                host = miUrl.getHost();
             }
             catch (Exception e) {
                 //Nada de nada
             }
+
+            contenidoCampo = contenidoCampo + "<a href='" + urlString + "'>"+ host +"</a><br>";
         }
-        System.out.println(contenidoCampo);
+        //System.out.println(contenidoCampo);
+        return contenidoCampo;
+    }
+
+    private void rellenarWebsTatuador(ArrayList<String> urls){
         TextView websTatuador = findViewById(R.id.websTatuador);
-        websTatuador.setText(Html.fromHtml(contenidoCampo));
+        websTatuador.setText(Html.fromHtml(crearContenidoHTML(urls)));
+    }
+
+    private void rellenarWebsEsrudio(ArrayList<String> urls){
+        TextView websEstudio = findViewById(R.id.websEstudio);
+        websEstudio.setText(Html.fromHtml(crearContenidoHTML(urls)));
     }
 
     @Override
