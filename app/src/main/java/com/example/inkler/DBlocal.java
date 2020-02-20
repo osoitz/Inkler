7b3b9f4615863166d651789ab7548e06aa92fdfa
mid-ra <mid-ra@users.noreply.github.com>
2020-02-20 10:19:33 +0100

    Modificar Tatuador recoge todos los datos

--- app/src/main/java/com/example/inkler/DBlocal.java
+++ app/src/main/java/com/example/inkler/DBlocal.java
@@ -1,8 +1,10 @@
 package com.example.inkler;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -160,4 +162,40 @@ public class DBlocal   {
         return webs;
     }
 
+    public int RecogerIdEstudio (String nombreEstudio){
+        int idEstudio=0;
+
+        //Columnas
+        String[] proyeccion = {DBHelper.entidadEstudio._ID};
+        String selection = DBHelper.entidadEstudio.COLUMN_NAME_NOMBRE + " = ?";
+        String[] selectionArgs = new String[] { "" + nombreEstudio } ;
+        //Respuesta
+        Cursor cursor = db.query(
+                DBHelper.entidadEstudio.TABLE_NAME,
+                proyeccion,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                null);
+        if (cursor.getCount()>0){
+            cursor.moveToFirst();
+
+            idEstudio= cursor.getInt(cursor.getColumnIndexOrThrow(DBHelper.entidadEstudio._ID));
+        }
+        cursor.close();
+
+
+
+        return idEstudio;
+    }
+
+    public void insertarTatuador(String st_nombre,String st_apellidos, String st_nombreArtistico, int IdEstudio){
+        ContentValues e1 = new ContentValues();
+        e1.put(DBHelper.entidadTatuador.COLUMN_NAME_NOMBRE, st_nombre);
+        e1.put(DBHelper.entidadTatuador.COLUMN_NAME_APELLIDOS, st_apellidos);
+        e1.put(DBHelper.entidadTatuador.COLUMN_NAME_NOMBRE_ARTISTICO, st_nombreArtistico);
+        e1.put(DBHelper.entidadTatuador.COLUMN_NAME_ID_ESTUDIO, IdEstudio);
+        db.insert(DBHelper.entidadTatuador.TABLE_NAME, null, e1);
+    }
 }
