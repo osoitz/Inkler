8d764e54f1522912732e9f93b4b5033b4fcbe6f2
BizkaiaBasketG2 <bizkaiabasketg2@gmail.com>
2020-04-10 12:20:37 +0200

    Eliminar "rellenar DB" de la pantalla inicial y poner unos segundos de espera

--- app/src/main/java/com/example/inkler/DBlocal.java
+++ app/src/main/java/com/example/inkler/DBlocal.java
@@ -28,7 +28,32 @@ class DBlocal   {
             this.db = dbHelper.getReadableDatabase();
         }
     }
-    
+
+    boolean checkEmpty(){
+        abrirDB(false);
+
+        int ntatuadores = 0;
+
+        boolean tatuadores = false;
+
+        //Columnas
+        String[] proyeccion = {DBHelper.entidadTatuador._ID};
+
+
+        //Respuesta
+        Cursor cursor = db.query(DBHelper.entidadTatuador.TABLE_NAME, proyeccion, null, null, null, null, null);
+
+        // recoger los datos
+        while (cursor.moveToNext()) {
+            ntatuadores++;
+        }
+        cursor.close();
+        db.close();
+        if (ntatuadores > 0){
+            tatuadores = true;
+        }
+        return tatuadores;
+    }
 
     List<Tatuador> recogerTatuadores(){
         abrirDB(false);
