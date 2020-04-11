6c23df6fb56455b50a4d08b2fa8d3c1be785d29b
BizkaiaBasketG2 <bizkaiabasketg2@gmail.com>
2020-04-11 13:10:51 +0200

    Gson

--- app/src/main/java/com/example/inkler/MainActivity.java
+++ app/src/main/java/com/example/inkler/MainActivity.java
@@ -7,12 +7,19 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 
+import com.google.gson.Gson;
+
 public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Gson gson = new Gson();
+        String jsonArray = "{'id':25,'nombreArtistico':'AKA','nombre':'Name','apellidos':'Surname','idEstudio':1}";
+        Tatuador t = gson.fromJson(jsonArray, Tatuador.class);
+        Log.d("AAA", "Nombre: " + t.getNombre() + " Apellidos: " + t.getApellidos() + " ID: " + t.getId() + " Nombre Artistico: " + t.getNombreArtistico() + " ID Estudio: " + t.getIdEstudio());
 
         DBlocal db = new DBlocal(getApplicationContext());
         boolean llena = db.checkEmpty();
