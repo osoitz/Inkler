12c74aa7aa5848a8b1f9f61998cdbdb2a54461ef
Osoitz Elkorobarrutia <osoitz@gmx.com>
2020-04-11 14:02:03 +0200

    JSON a medias

--- app/src/main/java/com/example/inkler/MainActivity.java
+++ app/src/main/java/com/example/inkler/MainActivity.java
@@ -9,12 +9,68 @@ import android.util.Log;
 
 import com.google.gson.Gson;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        InputStream inputStream = getResources().openRawResource(R.raw.datosejemplo);
+        BufferedReader bufferedReader= new BufferedReader(new InputStreamReader(inputStream));
+        String eachline = null;
+        String cadenaJSON = "";
+        try {
+            eachline = bufferedReader.readLine();
+            while (eachline != null) {
+                // `the words in the file are separated by space`, so to get each words
+                //String[] words = eachline.split(" ");
+                cadenaJSON += eachline;
+                eachline = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //System.out.println("CADENA JSON");
+        //System.out.println(cadenaJSON);
+
+        try {
+            JSONObject miJSON = new JSONObject(cadenaJSON);
+            JSONArray estudios = miJSON.getJSONArray("estudios");
+
+            for(int i=0;i<estudios.length();i++)
+            {
+                JSONObject estudio = estudios.getJSONObject(i);
+                System.out.println(estudio.getString("nombre"));
+                //....
+
+                JSONArray tatuadores = estudio.getJSONArray("tatuadores");
+
+                for(int j = 0; j < tatuadores.length(); j++) {
+                    JSONObject tatuador = tatuadores.getJSONObject(j);
+                    System.out.println(tatuador.getString("nombreArtistico"));
+                    //...
+
+                }
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+
+
 
         Gson gson = new Gson();
         String jsonArray = "{'id':25,'nombreArtistico':'AKA','nombre':'Name','apellidos':'Surname','idEstudio':1}";
