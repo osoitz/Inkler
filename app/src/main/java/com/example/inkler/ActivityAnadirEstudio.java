4fbac1c44802942d2135cab2cd3db7d234fd10d4
Osoitz Elkorobarrutia <osoitz@gmx.com>
2020-04-13 20:41:45 +0200

    OBJETOS!

--- app/src/main/java/com/example/inkler/ActivityAnadirEstudio.java
+++ app/src/main/java/com/example/inkler/ActivityAnadirEstudio.java
@@ -15,6 +15,13 @@ import android.widget.Toast;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ActivityAnadirEstudio extends AppCompatActivity {
+    EditText et_nombre;
+    EditText et_telefono;
+    EditText et_direccion;
+    EditText et_email;
+    EditText et_longitud;
+    EditText et_latitud;
+    int idEstudio;
 
 
     @Override
@@ -22,27 +29,25 @@ public class ActivityAnadirEstudio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anadir_estudio);
         final DBlocal db = new DBlocal(getApplicationContext());
-        final boolean anadir = getIntent().getBooleanExtra("añadir",false);
-        final EditText et_nombre = findViewById(R.id.contentNombre);
-        final EditText et_telefono = findViewById(R.id.contentTelefono);
-        final EditText et_direccion = findViewById(R.id.contentDireccion);
-        final EditText et_email = findViewById(R.id.contentMail);
-        final EditText et_longitud = findViewById(R.id.contentLongitud);
-        final EditText et_latitud = findViewById(R.id.contentLatitud);
+        Estudio estudio =  db.recogerEstudio(idEstudio);
 
-        FloatingActionButton fab = findViewById(R.id.btnAñadirEstudio);
+        et_nombre = findViewById(R.id.contentNombre);
+        et_telefono = findViewById(R.id.contentTelefono);
+        et_direccion = findViewById(R.id.contentDireccion);
+        et_email = findViewById(R.id.contentMail);
+        et_longitud = findViewById(R.id.contentLongitud);
+        et_latitud = findViewById(R.id.contentLatitud);
+
+        final FloatingActionButton botonGuardar = findViewById(R.id.btnAñadirEstudio);
         FloatingActionButton nuevaWeb = findViewById(R.id.estuAñadirWeb);
         nuevaWeb.setVisibility(View.GONE);
 
+        final boolean anadir = getIntent().getBooleanExtra("añadir",false);
+        idEstudio = getIntent().getIntExtra("idEstudio", -1);
+
         if (!anadir) {
-            final int idTatuador = App.getIdTatuador();
-            final int idEstudio = db.recogerTatuador(idTatuador).getIdEstudio();
-            et_nombre.setText(db.recogerEstudio(idTatuador).getNombre());
-            et_direccion.setText(db.recogerEstudio(idEstudio).getDireccion());
-            et_email.setText(db.recogerEstudio(idEstudio).getEmail());
-            et_telefono.setText(db.recogerEstudio(idEstudio).getTelefono());
-            et_longitud.setText(String.valueOf(db.recogerEstudio(idEstudio).getLongitud()));
-            et_latitud.setText(String.valueOf(db.recogerEstudio(idEstudio).getLatitud()));
+            //Estamos en modificar
+            rellenarDatos(estudio);
             nuevaWeb.setVisibility(View.VISIBLE);
             nuevaWeb.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -57,51 +62,71 @@ public class ActivityAnadirEstudio extends AppCompatActivity {
                     input.setLayoutParams(lp);
                     input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_URI);
                     alertDialog.setView(input);
-
                     alertDialog.setPositiveButton(getString(R.string.add), new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            String web = input.getText().toString();
-                            db.insertarWeb(idEstudio, web, idTatuador);
+                            Web web = new Web();
+                            web.setUrl(input.getText().toString());
+                            web.setIdEstudio(idEstudio);
+                            db.insertarWeb(web);
                         }
                     });
                     alertDialog.show();
                 }
             });
+
         }
 
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        botonGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String st_nombre = et_nombre.getText().toString();
-                String st_telefono = et_telefono.getText().toString();
-                String st_direccion = et_direccion.getText().toString();
-                String st_email = et_email.getText().toString();
-                String st_longitud = et_longitud.getText().toString();
-                String st_latitud = et_latitud.getText().toString();
+                Estudio nuevoEstudio = recogerDatos();
 
-                if (st_nombre.equals("") || st_direccion.equals("") || st_email.equals("") || st_latitud.equals("") || st_longitud.equals("") || st_telefono.equals("")) {
-                    Toast.makeText(getApplicationContext(), R.string.fill_all, Toast.LENGTH_LONG).show();
+                // Iniciar base de datos
+                if (anadir) {
+                    db.insertarEstudio(nuevoEstudio);
+                    Toast.makeText(getApplicationContext(), "El estudio " + nuevoEstudio.getNombre() + " ha sido creado", Toast.LENGTH_SHORT).show();
                 } else {
-                    double latitud = Double.parseDouble(st_latitud);
-                    double longitud = Double.parseDouble(st_longitud);
-                    // Iniciar base de datos
-                    if (anadir) {
-                        db.insertarEstudio(st_nombre, st_direccion, latitud, longitud, st_email, st_telefono);
-                        Toast.makeText(getApplicationContext(), "El estudio " + st_nombre + " ha sido creado", Toast.LENGTH_SHORT).show();
-                    } else {
-                        db.modificarEstudio(db.recogerTatuador(App.getIdTatuador()).getIdEstudio(), st_nombre, st_direccion, latitud, longitud, st_email, st_telefono);
-                        Toast.makeText(getApplicationContext(), "El estudio " + st_nombre + " ha sido modificado", Toast.LENGTH_SHORT).show();
-                    }
-                    Intent intent = new Intent(ActivityAnadirEstudio.this, ActivityListaTatuadores.class);
-                    startActivity(intent);
+                    nuevoEstudio.setIdEstudio(idEstudio);
+                    db.modificarEstudio(nuevoEstudio);
+                    Toast.makeText(getApplicationContext(), "El estudio " + nuevoEstudio.getNombre() + " ha sido modificado", Toast.LENGTH_SHORT).show();
                 }
+                Intent intent = new Intent(ActivityAnadirEstudio.this, ActivityListaTatuadores.class);
+                startActivity(intent);
             }
-        });
+
+    });
 
 
 
+
+}
+
+    private void rellenarDatos(Estudio estudio) {
+        et_nombre.setText(estudio.getNombre());
+        et_direccion.setText(estudio.getDireccion());
+        et_email.setText(estudio.getEmail());
+        et_telefono.setText(estudio.getTelefono());
+        et_longitud.setText(String.valueOf(estudio.getLongitud()));
+        et_latitud.setText(String.valueOf(estudio.getLatitud()));
     }
 
+    private Estudio recogerDatos() {
+    /*
+        if (
+                st_nombre.equals("") || st_direccion.equals("") || st_email.equals("") || st_latitud.equals("") || st_longitud.equals("") || st_telefono.equals("")) {
+            Toast.makeText(getApplicationContext(), R.string.fill_all, Toast.LENGTH_LONG).show();
+        } else {
 
+     */
+        Estudio estudio = new Estudio();
+        estudio.setNombre(et_nombre.getText().toString());
+        estudio.setTelefono(et_telefono.getText().toString());
+        estudio.setDireccion(et_direccion.getText().toString());
+        estudio.setEmail(et_email.getText().toString());
+        estudio.setLatitud(Double.parseDouble(et_latitud.getText().toString()));
+        estudio.setLongitud(Double.parseDouble(et_longitud.getText().toString()));
+        return estudio;
+    }
 }
